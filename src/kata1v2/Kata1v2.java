3067e5e3c5fd11ebd0c4bf3f030a540c48c3a796
jorgeortega <jorgeortega@10.230.173.186>
2016-09-23 11:57:59 +0100

    Kata 1 vesion 2 con Calendar y GregorianCalendar

---
+++ src/kata1v2/Kata1v2.java
@@ -0,0 +1,17 @@
+package kata1v2;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Kata1v2 {
+
+    public static void main(String[] args) {
+        Calendar date;
+        date = GregorianCalendar.getInstance();
+        date.set(1990, 6, 24);
+        
+        Person person = new Person("Jorge", date);
+        System.out.println(person.getName() + " tiene " + person.getAge() + " años.");
+    }
+    
+}
